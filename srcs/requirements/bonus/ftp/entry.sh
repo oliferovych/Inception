d2ff80e8aeb6137e6420907aa96e291e12af5b1b
oliferovych <dimasikolif@gmail.com>
2025-04-08 20:12:19 +0200

    added logging

--- srcs/requirements/bonus/ftp/entry.sh
+++ srcs/requirements/bonus/ftp/entry.sh
@@ -22,13 +22,22 @@ echo ${FTP_USER}
 echo ${FTP_PASSWORD}
 echo ${DOMAIN}
 
-useradd -m -d /home/$FTP_USER $FTP_USER
-echo "$FTP_USER:$FTP_PASSWORD" | chpasswd
-chown -R $FTP_USER:$FTP_USER /home/$FTP_USER
-chown -R $FTP_USER:$FTP_USER /var/www/html
+if id "$FTP_USER" &>/dev/null; then
+    echo "User $FTP_USER already exists."
+else
+    echo "Creating user $FTP_USER..."
+    useradd -m -d /home/$FTP_USER $FTP_USER
+    echo "$FTP_USER:$FTP_PASSWORD" | chpasswd
+    chown -R $FTP_USER:$FTP_USER /home/$FTP_USER
+    chown -R $FTP_USER:$FTP_USER /var/www/html
+fi
 
 mkdir -p /var/run/vsftpd/empty
 chmod 755 /var/run/vsftpd/empty
+
+mkdir -p /var/log/vsftpd
+touch /var/log/vsftpd/vsftpd.log
+chmod 755 /var/log/vsftpd /var/log/vsftpd/vsftpd.log
 
 echo "pasv_address=${DOMAIN}" >> /etc/vsftpd/vsftpd.conf
 
